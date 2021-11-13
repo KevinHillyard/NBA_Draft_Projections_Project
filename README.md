e04cfea6babf14d2a7aa14396f162c32573968ce
Brent George <brentdgeorge1@gmail.com>
2021-11-13 12:24:24 -0700

    csv to arff code

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # NBA_Draft_Projections_Project
 
 See requirements.txt for dependencies
+
+
+
+## Creating ARFF files from Web Parser CSV Files
+
+1. Edit the end of the file `csv_to_arff.py` to use the input file and output file names
+2. Run the following code
+
+```
+$ python3 csv_to_arff.py
+```
+
+3. The output should be `Nominal Values: {array of nominal values found}`
+4. Go to the output arff file and replace "string" attributes with values from the output of the python script
+4. Remove all occurences of "'" char (apostraphe)
--- csv_to_arff.py
+++ csv_to_arff.py
@@ -11,14 +11,34 @@ class CSV_to_ARFF:
         with open(self.in_file, newline="") as csvfile:
             data = list(csv.reader(csvfile))
 
+        nominal_values = [[] for i in range(len(data[1]))]
+
+        for i in range(len(data[1:])):
+            for j in range(len(data[i + 1])):
+                data[i + 1][j] = data[i + 1][j].replace(" ", "_")
+                data[i + 1][j] = data[i + 1][j].replace("cm", "")
+                data[i + 1][j] = data[i + 1][j].replace("kg", "")
+                try:
+                    data[i + 1][j] = int(data[i + 1][j])
+                except:
+                    try:
+                        data[i + 1][j] = float(data[i + 1][j])
+                    except:
+                        if data[i + 1][j] not in nominal_values[j]:
+                            nominal_values[j].append(data[i + 1][j])
+
         arff.dump(
             self.out_file,
             data[1:],
             relation=self.in_file,
-            names=data[0],
+            names=[attribute.replace("%", "_percent") for attribute in data[0]],
         )
+
+        print(f"Nominal Values: {nominal_values}")
 
 
 if __name__ == "__main__":
-    arff_writer = CSV_to_ARFF(in_filename=r"test.csv", out_filename=r"test.arff")
+    arff_writer = CSV_to_ARFF(
+        in_filename=r"allPlayers.csv", out_filename=r"allPlayers.arff"
+    )
     arff_writer.write_arff()
