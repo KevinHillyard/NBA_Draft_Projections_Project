cb85f674bfcf3829ac95a97b66a9f7b51f380c7d
Brent George <brentdgeorge1@gmail.com>
2021-11-18 14:09:35 -0700

    arff utils to read in player arff file and encode nominal values

---
+++ models/arff_utils.py
@@ -0,0 +1,56 @@
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.utils import shuffle
+import arff_reader
+import numpy as np
+import math
+
+
+class Arff_Utils:
+    def get_all_players_as_numpy():
+        data_arff = arff_reader.Arff(arff=r"./files/allPlayers.arff", label_count=1)
+        nominals = data_arff.get_nominal_idx()
+        for i in range(len(nominals)):
+            nominals[i] = [nominals[i], data_arff.unique_value_count(nominals[i])]
+        nominals = np.array(nominals)
+        data = data_arff.data
+        data = shuffle(data)
+
+        scaler = MinMaxScaler()
+        scaler.fit(data[:, : data_arff.features_count])
+        data_features = scaler.transform(data[:, : data_arff.features_count])
+
+        columns_needed = [
+            math.ceil(math.log(unique_values, 2)) for unique_values in nominals[:, 1]
+        ]
+        insert_columns = np.zeros((len(data), sum(columns_needed)))
+
+        for row_index in range(len(data_features)):
+            col_index = 0
+            for i in range(len(nominals)):
+                bits = format(
+                    int(
+                        data_features[row_index][nominals[i][0]] * (nominals[i][1] - 1)
+                    ),
+                    "b",
+                ).zfill(columns_needed[i])
+
+                for j in range(len(bits)):
+                    insert_columns[row_index][col_index] = bits[j]
+                    col_index += 1
+
+        tmp = np.array(data_features)
+        index_tracker = 0
+        for i in range(len(nominals)):
+            data_features = np.concatenate(
+                (
+                    data_features[:, : nominals[i][0] + index_tracker],
+                    insert_columns[
+                        :, index_tracker + i : index_tracker + columns_needed[i] + i
+                    ],
+                    data_features[:, nominals[i][0] + index_tracker + 1 :],
+                ),
+                axis=1,
+            )
+            index_tracker += columns_needed[i] - 1
+
+        return data_features, data[:, data_arff.features_count :]
--- models/mlp.py
+++ models/mlp.py
@@ -4,31 +4,33 @@ import csv
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import shuffle
-import arff_reader
+from arff_utils import Arff_Utils
 import math
 import sys
 
 
 def mlp():
-    np.set_printoptions(threshold=sys.maxsize)
-    data_arff = arff_reader.Arff(arff=r"./files/allPlayers.arff", label_count=1)
-    data = data_arff.data
-    data = shuffle(data)
+    x, y = Arff_Utils.get_all_players_as_numpy()
 
-    scaler = MinMaxScaler()
-    scaler.fit(data[:, : data_arff.features_count])
-    data_features = scaler.transform(data[:, : data_arff.features_count])
+    training_index = round(len(x) * 0.8)
+    training_x = x[:training_index]
+    test_x = x[training_index:]
+    training_y = y[:training_index, :]
+    test_y = y[training_index:, :]
 
-    training_index = round(len(data_features) * 0.8)
-    training_x = data_features[:training_index]
-    test_x = data_features[training_index:]
-    training_y = data[:training_index, data_arff.features_count :]
-    test_y = data[training_index:, data_arff.features_count :]
-
-    hidden_nodes_options = [(32), (64), (128), (8, 8), (16, 16), (32, 32)]
+    hidden_nodes_options = [
+        (32),
+        (64),
+        (128),
+        (8, 8),
+        (16, 16),
+        (32, 32),
+        (8, 16, 8),
+        (16, 32, 16),
+        (8, 8, 8),
+    ]
     lrs = [1.0, 0.8, 0.1, 0.01]
     momentums = [0.0, 0.5, 0.8, 1.0]
-    nesterov = [True, False]
     regularization = [0, 0.00001, 0.0001, 0.0005]
     with open(
         r"./files/grid_search.csv",
@@ -44,48 +46,44 @@ def mlp():
                 "Hidden Nodes",
                 "Learning Rate",
                 "Momentum",
-                "Nesterov",
                 "Regularization",
             ]
         )
         for hn_ in hidden_nodes_options:
             for lr_ in lrs:
                 for momentum_ in momentums:
-                    for nest_ in nesterov:
-                        for reg_ in regularization:
-                            mlp_model = MLPClassifier(
-                                hidden_layer_sizes=hn_,
-                                alpha=reg_,
-                                learning_rate_init=lr_,
-                                max_iter=10000000,
-                                momentum=momentum_,
-                                nesterovs_momentum=nest_,
-                            )
-                            mlp_model.fit(
-                                training_x,
-                                training_y.flatten(),
-                            )
-                            predictions = mlp_model.predict(test_x)
-                            y = test_y.flatten()
-                            mse = np.zeros(len(y))
-                            for i in range(len(predictions)):
-                                prediction = predictions[i]
-                                if prediction > 61:
-                                    prediction == 61
-                                mse[i] = (prediction - y[i]) ** 2
+                    for reg_ in regularization:
+                        mlp_model = MLPClassifier(
+                            hidden_layer_sizes=hn_,
+                            alpha=reg_,
+                            learning_rate_init=lr_,
+                            max_iter=10000000,
+                            momentum=momentum_,
+                        )
+                        mlp_model.fit(
+                            training_x,
+                            training_y.flatten(),
+                        )
+                        predictions = mlp_model.predict(test_x)
+                        y = test_y.flatten()
+                        mse = np.zeros(len(y))
+                        for i in range(len(predictions)):
+                            prediction = predictions[i]
+                            if prediction > 61:
+                                prediction == 61
+                            mse[i] = (prediction - y[i]) ** 2
 
-                            mse = np.sum(mse) / len(mse)
-                            rmse = math.sqrt(mse)
-                            result_row = [
-                                rmse,
-                                hn_,
-                                lr_,
-                                momentum_,
-                                nest_,
-                                reg_,
-                            ]
-                            writer.writerow(result_row)
-                            print(f"{result_row}")
+                        mse = np.sum(mse) / len(mse)
+                        rmse = math.sqrt(mse)
+                        result_row = [
+                            rmse,
+                            hn_,
+                            lr_,
+                            momentum_,
+                            reg_,
+                        ]
+                        writer.writerow(result_row)
+                        print(f"{result_row}")
 
 
 if __name__ == "__main__":
